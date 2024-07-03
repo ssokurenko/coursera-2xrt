add7b875f142caf934d960fe95c0ebae22e5bd7f
ssokurenko <s.sokurenko@gmail.com>
2024-07-03 15:38:53 -0600

    Project

---
+++ src/components/Header.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faMedium,
+  faStackOverflow,
+} from "@fortawesome/free-brands-svg-icons";
+import { Box, HStack } from "@chakra-ui/react";
+
+const socials = [
+  {
+    icon: faEnvelope,
+    url: "mailto: hello@example.com",
+  },
+  {
+    icon: faGithub,
+    url: "https://github.com",
+  },
+  {
+    icon: faLinkedin,
+    url: "https://www.linkedin.com",
+  },
+  {
+    icon: faMedium,
+    url: "https://medium.com",
+  },
+  {
+    icon: faStackOverflow,
+    url: "https://stackoverflow.com",
+  },
+];
+
+const Header = () => {
+  const handleClick = (e, anchor) => {
+    e.preventDefault()
+    const id = `${anchor}-section`;
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
+  return (
+    <Box
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      translateY={0}
+      transitionProperty="transform"
+      transitionDuration=".3s"
+      transitionTimingFunction="ease-in-out"
+      backgroundColor="#18181b"
+    >
+      <Box color="white" maxWidth="1280px" margin="0 auto">
+        <HStack
+          px={16}
+          py={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <nav>
+            {socials.map(i => <a href={i.url} key={i.url} style={{marginRight: '1rem'}}><FontAwesomeIcon icon={i.icon} size="2x" /></a>)}
+          </nav>
+          <nav>
+            <HStack spacing={8}>
+              <a href="#" onClick={e => handleClick(e, 'projects')}>Projects</a>
+              <a href="#" onClick={e => handleClick(e, 'contactme')}>Contact me</a>
+            </HStack>
+          </nav>
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+export default Header;
---
+++ src/components/LandingSection.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hello, I am Serge!";
+const bio1 = "A frontend developer";
+const bio2 = "React rocks 🚀";
+
+
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+    <VStack>
+      <Avatar name="Pit" src="https://i.pravatar.cc/150?img=12" size="3xl" />
+      <Heading>{greeting}</Heading>
+      <Heading>{bio1}</Heading>
+      <Heading>{bio2}</Heading>
+    </VStack>
+  </FullScreenSection>
+);
+
+export default LandingSection;
